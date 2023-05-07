32cedcb14bd53df6c349d2da39f1b2e4409bcafa
George Rashed <goerge1110@gmail.com>
2023-05-08 00:36:37 +0300

    Added Navigation, Face & Move to Dest, with Y too.

--- AI Final Project/Assets/Scripts/AI/DummyTestAI.cs
+++ AI Final Project/Assets/Scripts/AI/DummyTestAI.cs
@@ -7,8 +7,10 @@ public class DummyTestAI : MonoBehaviour
     [Header("Movement")]
     public Vector3 Direction;
     public float Speed;
+    public float AngularSpeed;
     private Vector3 _movingVelocity;
     private bool _isMoving;
+    private bool _canMove;
 
     [Header("Gravity")]
     public bool UseGravity;
@@ -19,11 +21,14 @@ public class DummyTestAI : MonoBehaviour
     private bool _isGrounded;
     private Vector3 _gravityVelocityEffect;
 
+    [Header("Navigation")]
+    public bool UseNav = true;
+    public Vector3 Destination;
+    public float StoppingDistance = 0.1f;
+    private float _targetDistance;
+    private float _targetDistanceNoHeight;
+    private bool _reachedDestination;
 
-    private void OnValidate()
-    {
-        Direction = Direction.normalized;
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -34,10 +39,10 @@ public class DummyTestAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Direction = Direction.normalized;
-
-        if (_movingVelocity.magnitude != 0) _isMoving = true;
+        if (_movingVelocity.magnitude != 0 && _canMove) _isMoving = true;
         else _isMoving = false;
+
+        GoToDestination();
     }
 
     private void FixedUpdate()
@@ -49,16 +54,33 @@ public class DummyTestAI : MonoBehaviour
 
     private void MovementHandler()
     {
-        Vector3 moveDir = ((transform.right * Direction.x) + (transform.up * Direction.y) + (transform.forward * Direction.z)).normalized;
-        _movingVelocity = moveDir * (Speed * Time.fixedDeltaTime);
-        transform.position += _movingVelocity;
+        if (_canMove)
+        {
+            if (transform.position.y > Destination.y + StoppingDistance)
+            {
+                Direction.y = -1;
+            }
+            else if (transform.position.y < Destination.y - StoppingDistance)
+            {
+                Direction.y = 1;
+            }
+            else
+            {
+                Direction.y = 0;
+            }
+
+            Vector3 moveDir = ((transform.right * Direction.x) + (transform.up * Direction.y) + (transform.forward * Direction.z)).normalized;
+            _movingVelocity = moveDir * (Speed * Time.fixedDeltaTime);
+            transform.position += _movingVelocity;
+        }
     }
 
     private void GravityHandler()
     {
+        _isGrounded = Physics.CheckBox(transform.position + GroundCheckOffset, GroundedBoxSize / 2, Quaternion.identity, GroundLayer);
+
         if (UseGravity)
         {
-            _isGrounded = Physics.CheckBox(transform.position + GroundCheckOffset, GroundedBoxSize / 2, Quaternion.identity, GroundLayer);
 
             if (_isGrounded && _gravityVelocityEffect.y <= 0)
             {
@@ -76,6 +98,58 @@ public class DummyTestAI : MonoBehaviour
         }
     }
 
+    private void MathDistances()
+    {
+        Vector3 posYZero = transform.position;
+        posYZero.y = 0;
+        Vector3 destYZero = Destination;
+        destYZero.y = 0;
+
+        _targetDistance = Vector3.Distance(transform.position, Destination);
+        _targetDistanceNoHeight = Vector3.Distance(posYZero, destYZero);
+    }
+
+    private void GoToDestination()
+    {
+        if (UseNav)
+        {
+            MathDistances();
+
+            if (_targetDistance < StoppingDistance)
+            {
+                _reachedDestination = true;
+            }
+            else
+            {
+                _reachedDestination = false;
+            }
+
+            if (!_reachedDestination)
+            {
+                FaceTarget();
+                _canMove = true;
+            }
+            else
+            {
+                _canMove = false;
+            }
+        }
+        else
+        {
+            _canMove = true;
+        }
+    }
+
+    private void FaceTarget()
+    {
+        if (_targetDistanceNoHeight >= StoppingDistance)
+        {
+            Vector3 lookPos = (Destination - transform.position).normalized;
+            lookPos.y = 0;
+            Quaternion rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * AngularSpeed);
+        }
+    }
 
     private void OnDrawGizmos()
     {
