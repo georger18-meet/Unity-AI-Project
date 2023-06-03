5af021df127ef727cfd828d3b23feb521a432aa7
George Rashed <goerge1110@gmail.com>
2023-06-04 00:31:28 +0300

    Made Heap Structures.

--- AI Final Project/Assets/Scripts/Pathfinding/Node.cs
+++ AI Final Project/Assets/Scripts/Pathfinding/Node.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node
+public class Node : IHeapItem<Node>
 {
     public bool Walkable;
     public Vector3 WorldPosition;
@@ -12,7 +12,10 @@ public class Node
     public Node ParentNode;
     public int GCost;
     public int HCost;
+    int _heapIndex;
+
     public int FCost { get { return GCost + HCost; } }
+    public int HeapIndex { get => _heapIndex; set => _heapIndex = value; }
 
     public Node(bool walkable, Vector3 worldPosition, int gridX, int gridY)
     {
@@ -20,5 +23,15 @@ public class Node
         WorldPosition = worldPosition;
         GridX = gridX;
         GridY = gridY;
+    }
+
+    public int CompareTo(Node nodeToCompare)
+    {
+        int compare = FCost.CompareTo(nodeToCompare.FCost);
+        if (compare == 0)
+        {
+            compare = HCost.CompareTo(nodeToCompare.HCost);
+        }
+        return -compare;
     }
 }
